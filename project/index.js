4d761295e3752c202311a9d411508b36a1140944
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2024-02-28 10:16:11 +0530

    introduced cors

--- project/index.js
+++ project/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const mongoFuncs = require('./models/mongowala.js');
 const dummyData = require('./data/products.json');
+const cors = require('cors')
 
 const app = express();
 
@@ -12,6 +13,7 @@ mongoose.connect('mongodb+srv://Ayush:2BXNWx4qaZkr3C5y@cluster0.ijs1ymf.mongodb.
 
 
 app.use(express.json());
+app.use(cors());
 
 
 
